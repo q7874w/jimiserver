2f22afa57c21a0527112f0cc2aca57f35c17c3ec
q7874w <787486929@qq.com>
2018-06-01 16:45:31 +0800

    2

---
+++ routes/mysql.js
@@ -0,0 +1,11 @@
+var mysql=require("mysql");
+
+var connect=mysql.createConnection({
+    host:"localhost",
+    port:"3306",
+    user:"root",
+    password:"",
+    database:"localjimi"
+});
+
+module.exports=connect;
